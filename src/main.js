369c7b3ab267a1809375b503289a31d707d0942d
Lina Alvarado <linamariaalvaradopallares@gmail.com>
2022-08-02 10:25:09 -0500

    Pruebas

--- src/main.js
+++ src/main.js
@@ -9,11 +9,18 @@ for (let i = 0; i < 2023; i++) {
   document.getElementById("atletas").innerHTML = olympic[0];
 
 }*/
+
+let eleccion= document.getElementById("team")
 let atletasContenedor = document.getElementById("atletas");
+let todosAtletas = olymGam.athletes.slice(0, 10);
+
+let filtrado= todosAtletas.filter( todosAtletas => todosAtletas.team === "Russia")
+console.log(filtrado);
 // const todosAtletas = olymGam.athletes;
-const todosAtletas = olymGam.athletes.slice(0, 10);
-for (let atleta of todosAtletas) {
-  console.log(atleta);
+
+
+// for (let atleta of todosAtletas) {
+//   console.log(atleta);
   //   ************ MANIPULACION DINAMICA DEL DOM
   //   const p = document.createElement("p");
   //   const article = document.createElement("article");
@@ -22,17 +29,19 @@ for (let atleta of todosAtletas) {
   //   atletasContenedor.append(article);
 
   //   ************ template string && interpolacion de variables
-  //   atletasContenedor.innerHTML += `<article>
-  //       <h3>${atleta.name}</h3>
-  //       <p></p>
-  //       <img src="" alt="" srcset="">
-  //     </article>`;
+    // atletasContenedor.innerHTML += `<article>
+    //     <h3>${atleta.name}</h3>
+    //     <p></p>
+    //     <img src="" alt="" srcset="">
+    //   </article>`;
   // atletasContenedor.innerHTML += atleta.name;
+  // atletasContenedor.innerHTML += `<h3>${atleta.name}-- ${atleta.team}</h3>`;
+  
 
   //    = Object.values(
   //     olymGam.athletes[prop]
   //   );
-}
+// }
 // console.log(olymGam.athletes);
 
 // console.log(example, data);
