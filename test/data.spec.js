fe472e4a1da7848f3751b9a69dd312c4a5109ba4
Daniela Nieto <yudadaniela@hotmail.com>
2022-08-11 14:11:37 -0500

    testejemplo

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,29 @@
-import { example, anotherExample } from '../src/data.js';
+import { filtrado } from "../src/data.js";
+import dataMock from "./datamock.js";
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe("example", () => {
+  // FN FILTRADO
+  it("filtrado es una funcion", () => {
+    expect(typeof filtrado).toBe("function");
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+  it("return de filtrado o yo que se", () => {
+    expect(
+      filtrado(dataMock.array, dataMock.input, dataMock.categoria)
+    ).toStrictEqual(dataMock.returnFiltrado);
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it.skip("returns `example`", () => {
+    expect(filtrado()).toBe("example");
   });
 });
+
+// describe("anotherExample", () => {
+//   it("is a function", () => {
+//     expect(typeof anotherExample).toBe("function");
+//   });
+
+//   it("returns `anotherExample`", () => {
+//     expect(anotherExample()).toBe("OMG");
+//   });
+// });
